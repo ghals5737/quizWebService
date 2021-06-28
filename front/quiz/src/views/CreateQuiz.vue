1433e03ca15e5032c3a14a6a8a5eb229d30d9efb
ghals5737 <ghals5737@naver.com>
2021-06-29 08:22:28 +0900

    20210628

--- front/quiz/src/views/CreateQuiz.vue
+++ front/quiz/src/views/CreateQuiz.vue
@@ -1,5 +1,113 @@
 <template>
+<v-app>
+<v-container>
+  <v-row>
+      <v-col md="2">
+        <v-navigation-drawer
+          floating
+          permanent
+        >
+          <v-list
+            dense
+            rounded
+          >
+            <v-list-item
+              v-for="item in items"
+              :key="item.title"
+              link
+            >
+              <v-list-item-icon>
+                <v-icon>{{ item.icon }}</v-icon>
+              </v-list-item-icon>
 
+              <v-list-item-content>
+                <v-list-item-title>aa</v-list-item-title>
+              </v-list-item-content>
+            </v-list-item>
+          </v-list>
+        </v-navigation-drawer>  
+      </v-col>
+      <v-col>
+      <v-text-field label="퀴즈 제목 입력"></v-text-field>            
+      <!--<div class="btn btn-group">   -->   
+      <v-container>
+        <v-row          
+          justify="left"
+        >
+        <v-col cols="auto">
+         <v-select 
+              depressed
+              v-model="mod"
+              label="퀴즈유형"
+              :items="options"
+              item-text="name"
+              item-value="value"
+              ></v-select>
+        </v-col>
+        </v-row>
+        <v-row          
+          justify="left"
+        >
+          <v-col cols="auto">
+            <v-btn depressed color="primary">Insert Question</v-btn>
+          </v-col>
+          <v-col cols="auto">
+              <v-btn depressed color="error">Delete Last Question</v-btn>
+          </v-col>
+          <v-col cols="auto">             
+          </v-col>                    
+        </v-row>
+      </v-container>  
+      <!--</div>      -->
+      
+      <v-divider></v-divider>
+      <v-form v-model="valid">
+      <v-container>                 
+         <v-card >
+          <v-subheader dark class="blue text--white">문제 1</v-subheader>  
+              <v-divider></v-divider>
+           <v-container fluid>
+            <v-row >
+             <v-col cols="auto">
+              <v-checkbox class="d-flex" v-model="checkbox1"></v-checkbox>
+             </v-col>
+             <v-col>
+              <v-text-field label="Include files"></v-text-field>
+             </v-col>
+            </v-row> 
+            <v-row >
+             <v-col cols="auto">
+              <v-checkbox class="d-flex" v-model="checkbox1"></v-checkbox>
+             </v-col>
+             <v-col>
+              <v-text-field label="Include files"></v-text-field>
+             </v-col>
+            </v-row> 
+            <v-row >
+             <v-col cols="auto">
+              <v-checkbox class="d-flex" v-model="checkbox1"></v-checkbox>
+             </v-col>
+             <v-col>
+              <v-text-field label="Include files"></v-text-field>
+             </v-col>
+            </v-row> 
+            <v-row >
+             <v-col cols="auto">
+              <v-checkbox class="d-flex" v-model="checkbox1"></v-checkbox>
+             </v-col>
+             <v-col>
+              <v-text-field label="Include files"></v-text-field>
+             </v-col>
+            </v-row> 
+          </v-container>
+        </v-card>       
+      </v-container>
+    </v-form>
+    </v-col>
+  </v-row>
+
+</v-container>
+</v-app>
 </template>
 
 <script>
@@ -13,6 +121,15 @@ export default {
     },
     data(){
       return{
+        items:[
+          {title:"aa"}
+        ],
+        options:[
+          {name: "객관식",value:1},
+          {name: "주관식",value:2}
+        ],
+        mod:0,
+        order:0,
         quizList:[],
         quiz:{
             problemList: [
