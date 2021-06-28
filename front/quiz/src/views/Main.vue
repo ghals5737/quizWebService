b0d90d773cc900541eea2f4195ec22366e346fa3
ghals5737 <ghals5737@naver.com>
2021-06-28 19:35:24 +0900

    20210628

---
+++ front/quiz/src/views/Main.vue
@@ -0,0 +1,55 @@
+<template>
+  <v-app>   
+      <div class="my-2">
+        <v-btn small @click="goCreateQuiz">퀴즈생성</v-btn>
+      </div>
+      <div class="my-2">
+        <v-btn small color="primary" @click="goSearchQuiz">퀴즈검색</v-btn>
+      </div>
+      <div class="my-2">
+        <v-btn small color="error">Error</v-btn>
+      </div>
+      <div class="my-2">
+        <v-btn small disabled>Disabled</v-btn>
+      </div>      
+  </v-app>
+</template>
+
+<script>
+import { mapGetters } from 'vuex'
+export default {
+    name: 'Main',
+    components: {      
+    },
+    computed: {
+         ...mapGetters(["USER"]),
+    },
+    data(){
+      return{
+        userId:'',
+        pw:'',
+      }
+    },
+    methods:{
+        goCreateQuiz(){
+            this.$store.dispatch("goCreateQuiz",{
+                userId: USER.userId,
+                userNo: USER.userNo
+            })  
+        },
+        goSearchQuiz(){
+            this.$store.dispatch("goSearchQuiz",{
+                userId: USER.userId,
+                userNo: USER.userNo
+            })  
+        }              
+    },
+    created(){
+        this.userId=sessionStorage.getItem("userId")
+    },
+}
+</script>
+
+<style>
+
+</style>
