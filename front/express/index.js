5857b71573639fbd064dc0d3e4f53d75419812c0
ghals5737 <ghals5737@naver.com>
2021-07-02 14:15:14 +0900

    index.html 수정

--- front/express/index.js
+++ front/express/index.js
@@ -1,27 +1,9 @@
 var express = require('express');
 var path = require('path');
-var router = express.Router();
 var app = express();
-
-var port = normalizePort(process.env.PORT || '8800');
-function normalizePort(val) {
-    var port = parseInt(val, 10);
-  
-    if (isNaN(port)) {
-      // named pipe
-      return val;
-    }
-  
-    if (port >= 0) {
-      // port number
-      return port;
-    }
-  
-    return false;
-  }
+var port = 8800
 /* GET home page. */
-router.get('/', function (req, res, next) {
+app.get('/', function (req, res, next) {
     res.sendFile(path.join(__dirname, '../quiz/dist', 'index.html'));
 });
-
-module.exports = router;
+app.listen(port,()=>{})
