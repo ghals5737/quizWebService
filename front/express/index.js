db65139e417d6871cd01b00da66e81224592e7ff
ghals5737 <ghals5737@naver.com>
2021-07-02 15:09:03 +0900

    add express

--- front/express/index.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var path = require('path');
-var app = express();
-var port = 8800
-/* GET home page. */
-app.get('/', function (req, res, next) {
-    res.sendFile(path.join(__dirname, '../quiz/dist', 'index.html'));
-});
-app.listen(port,()=>{})
