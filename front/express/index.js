b0b6b3de050412cab96eade2bfecba576e0caf1b
ghals5737 <ghals5737@naver.com>
2021-07-02 14:06:28 +0900

    update express

--- front/express/index.js
+++ front/express/index.js
@@ -4,10 +4,24 @@ var router = express.Router();
 var app = express();
 
 var port = normalizePort(process.env.PORT || '8800');
-
+function normalizePort(val) {
+    var port = parseInt(val, 10);
+  
+    if (isNaN(port)) {
+      // named pipe
+      return val;
+    }
+  
+    if (port >= 0) {
+      // port number
+      return port;
+    }
+  
+    return false;
+  }
 /* GET home page. */
 router.get('/', function (req, res, next) {
-    res.sendFile(path.join(__dirname, '../public', 'index.html'));
+    res.sendFile(path.join(__dirname, '../quiz/dist', 'index.html'));
 });
 
 module.exports = router;
