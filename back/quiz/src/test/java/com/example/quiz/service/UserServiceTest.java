88544ba43198c88a3a035e063b1915fe47a5623f
aaa <ghals5737@naver.com>
2022-10-21 17:43:40 +0900

    [refactoring]<feat> user service 조회 테스트 추가

--- back/quiz/src/test/java/com/example/quiz/service/UserServiceTest.java
+++ back/quiz/src/test/java/com/example/quiz/service/UserServiceTest.java
@@ -7,34 +7,17 @@ import lombok.NoArgsConstructor;
 import org.junit.jupiter.api.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-
 import java.time.LocalDateTime;
+import java.util.List;
+import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
 
 @SpringBootTest
-@AllArgsConstructor
 @NoArgsConstructor
 public class UserServiceTest {
     @Autowired
     private UserService userService;
     @Autowired
     private UserRepository userRepository;
-
-    private User saveTestUser;
-
-    @BeforeEach
-    public void init(){
-        saveTestUser=User.builder()
-                .userId("saveTest")
-                .userPw("saveTest")
-                .regTime(LocalDateTime.now())
-                .build();
-        userRepository.save(
-                User.builder()
-                .userId("test")
-                .userPw("test")
-                .regTime(LocalDateTime.now())
-                .build());
-    }
 
     @AfterEach
     public void clear(){
@@ -44,7 +27,52 @@ public class UserServiceTest {
     @Test
     @DisplayName("회원 가입이 정상 작동한다")
     void createUserTest(){
+        //given
+        User saveTestUser=User.builder()
+                .userId("saveTest")
+                .userPw("saveTest")
+                .regTime(LocalDateTime.now())
+                .build();
+        //when
         userRepository.save(saveTestUser);
+
+        //then
+        List<User> results=userRepository.findAll();
+        assertThat(results).hasSize(1);
+        assertThat(results.get(0).getUserId()).isEqualTo(saveTestUser.getUserId());
+        assertThat(results.get(0).getUserPw()).isEqualTo(saveTestUser.getUserPw());
     }
+
+    @Test
+    @DisplayName("유저 전체 조회가 정상 작동한다.")
+    void getAllUsers(){
+        //given
+        User saveTestUserA=User.builder()
+                .userId("saveTest")
+                .userPw("saveTest")
+                .regTime(LocalDateTime.now())
+                .build();
+        User saveTestUserB=User.builder()
+                .userId("saveTest")
+                .userPw("saveTest")
+                .regTime(LocalDateTime.now())
+                .build();
+        userRepository.save(saveTestUserA);
+        userRepository.save(saveTestUserB);
+
+        //when
+        List<User> results=userRepository.findAll();
+
+        //then
+        assertThat(results).hasSize(2);
+        assertThat(results)
+                .extracting("userId")
+                .containsExactlyInAnyOrder(saveTestUserA.getUserId(),saveTestUserB.getUserId());
+        assertThat(results)
+                .extracting("userPw")
+                .containsExactlyInAnyOrder(saveTestUserA.getUserPw(),saveTestUserB.getUserPw());
+    }
+
+
 
 }
